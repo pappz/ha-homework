7183d579ba95691fa9f276e695dbe24c0e36fa2e
Zoltan Papp <zoltan.papp@webkeyapp.com>
2022-11-14 17:42:23 +0100

    Small changes in comment

--- web/middleware/json.go
+++ web/middleware/json.go
@@ -1,6 +1,6 @@
 package middleware
 
-// Json general interface what represent the request parameters
+// Json general interface what represent the request payload
 type Json interface {
 	Validate() error
 }
